d35bf224c3af982cf975627222cc79dd5e3c8b1a
mkrivushin <mkrivushin@yandex.ru>
2008-10-31 00:23:56 +0600

    Now show progress, not bar : )

--- contents/code/main.py
+++ contents/code/main.py
@@ -9,6 +9,21 @@ from PyKDE4.plasma import Plasma
 import plasma                   
 import dbus
 
+class Player:
+    def __getattr__(self,name):
+        #print 'point %s'%name
+        def tryf():
+            try:
+                amarok = dbus.SessionBus().get_object('org.kde.amarok','/Player')
+                a=getattr(amarok,name)
+                return a()
+            except: pass
+        return tryf
+
+class MCLabel(Plasma.Label):
+    def mousePressEvent(self,event):
+        self.emit(SIGNAL('clicked'),event)
+
 class PyMCApplet(plasma.Applet):
     def __init__(self,parent,args=None):
         plasma.Applet.__init__(self,parent)
@@ -20,22 +35,25 @@ class PyMCApplet(plasma.Applet):
         self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
 
         self.dialog = None
-
-        self.bus = dbus.SessionBus()
-        self.amarok = self.bus.get_object('org.kde.amarok','/Player')
+        self.player = Player()
 
         self.theme = Plasma.Svg(self)
         self.theme.setImagePath("widgets/background")
         self.theme.setContainsMultipleImages(False)
         #self.theme.resize(self.size())
 
+        self.timer = self.startTimer(1000)
+
         self.mlayout = QGraphicsLinearLayout(Qt.Horizontal)
-        #self.eline = Plasma.LineEdit()
-        #self.mlayout.addItem(self.eline)
+        self.label = MCLabel()
+        self.label.setText('40%')
+
+        self.mlayout.addItem(self.label)
         self.prev_bt = Plasma.PushButton()
         self.prev_bt.setText('<-')
         self.play_bt = Plasma.PushButton()
         self.play_bt.setText('>')
+        self.play_bt.setImage('/usr/share/icons/oxygen/32x32/actions/media-playback-start.png')
         self.stop_bt = Plasma.PushButton()
         self.stop_bt.setText('-')
         self.next_bt = Plasma.PushButton()
@@ -52,22 +70,30 @@ class PyMCApplet(plasma.Applet):
         self.connect(self.stop_bt,SIGNAL('clicked()'),self.Stop)
         self.connect(self.next_bt,SIGNAL('clicked()'),self.Next)
 
+        self.connect(self.label,SIGNAL('clicked'),self.l_clicked)
+
+    def timerEvent(self,ev):
+        pos = int(self.player.PositionGet())
+        len = int(self.player.GetMetadata()['mtime'])
+        self.label.setText(str(pos*100/len)+'%')
+
+    def l_clicked(self,event):
+        print 'ku'
+        pos = int(self.player.PositionGet())
+        len = int(self.player.GetMetadata()['mtime'])
+        self.label.setText(str(pos*100/len)+'%')
 
     def Prev(self):
-        print "Prev it!"
-        self.amarok.Prev()
+        self.player.Prev()
 
     def Play(self):
-        print "Play it!"
-        self.amarok.Play()
+        self.player.Play()
 
     def Stop(self):
-        print "Stop it!"
-        self.amarok.Stop()
+        self.player.Stop()
 
     def Next(self):
-        print "Next it!"
-        self.amarok.Next()
+        self.player.Next()
 
     def shape(self):
         if self.theme.hasElement("hint-square-clock"):

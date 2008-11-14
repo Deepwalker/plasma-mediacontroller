275c621a263374fb6576b0508ce118365ca33734
krivushinme <mkrivushin@yandex.ru>
2008-11-14 12:30:35 +0600

    Volume and position controlled by mouse wheel.

--- contents/code/main.py
+++ contents/code/main.py
@@ -10,19 +10,24 @@ import plasma
 import dbus
 
 class Player:
+    def __init__(self,player):
+        self.player = player
+
     def __getattr__(self,name):
-        #print 'point %s'%name
-        def tryf():
+        def tryf(*args,**kwargs):
             try:
-                amarok = dbus.SessionBus().get_object('org.kde.amarok','/Player')
+                amarok = dbus.SessionBus().get_object(self.player,'/Player')
                 a=getattr(amarok,name)
-                return a()
+                return a(*args,**kwargs)
             except: pass
         return tryf
 
 class MCLabel(Plasma.Label):
     def mousePressEvent(self,event):
         self.emit(SIGNAL('clicked'),event)
+    def wheelEvent(self,event):
+        print event.delta()
+        self.emit(SIGNAL('wheel'),event)
 
 class PyMCApplet(plasma.Applet):
     def __init__(self,parent,args=None):
@@ -33,23 +38,26 @@ class PyMCApplet(plasma.Applet):
 
         self.setHasConfigurationInterface(False)
         self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
-        self.setMinimumSize(32*9,64)
+        self.setMinimumSize(32*8,64)
 
         self.dialog = None
-        self.player = Player()
+        self.player = Player('org.kde.amarok')
 
         self.theme = Plasma.Svg(self)
         self.theme.setImagePath("widgets/background")
         self.theme.setContainsMultipleImages(False)
-        #self.theme.resize(self.size())
 
         self.timer = self.startTimer(1000)
 
         self.mlayout = QGraphicsLinearLayout(Qt.Horizontal)
         self.label = MCLabel()
-        self.label.setText('40%')
+        self.label.setText('P:0%')
+
+        self.volume = MCLabel()
+        self.volume.setText('V:0%')
 
         self.mlayout.addItem(self.label)
+        self.mlayout.addItem(self.volume)
         self.prev_bt = MCLabel()
         self.prev_bt.setImage('/usr/share/icons/oxygen/32x32/actions/media-skip-backward.png')
         self.play_bt = MCLabel()
@@ -70,14 +78,42 @@ class PyMCApplet(plasma.Applet):
         self.connect(self.stop_bt,SIGNAL('clicked'),self.Stop)
         self.connect(self.next_bt,SIGNAL('clicked'),self.Next)
 
-        #self.connect(self.label,SIGNAL('clicked'),self.l_clicked)
+        self.connect(self.label,SIGNAL('wheel'),self.change_pos)
+        self.connect(self.volume,SIGNAL('wheel'),self.change_vol)
+
+    def redraw_vol(self,vol=None):
+        if not vol:
+            vol = int(self.player.VolumeGet())
+        self.volume.setText("V:%s%%"%str(vol))
+
+    def redraw_pos(self,pos=None):
+        if not pos:
+            try:
+                pos = int(self.player.PositionGet())
+                len = int(self.player.GetMetadata()['mtime'])
+                pos = pos*100/len
+            except:
+                pos=0
+        self.label.setText("P:%s%%"%str(pos))
 
     def timerEvent(self,ev):
-        try:
-            pos = int(self.player.PositionGet())
-            len = int(self.player.GetMetadata()['mtime'])
-            self.label.setText(str(pos*100/len)+'%')
-        except: pass
+        self.redraw_pos()
+        self.redraw_vol()
+
+    def change_vol(self,event):
+        delta = event.delta()/120
+        vol = self.player.VolumeGet()
+        vol += 5*delta
+        self.player.VolumeSet(vol)
+        self.redraw_vol(vol=vol)
+
+    def change_pos(self,event):
+        delta = event.delta()/120
+        pos = self.player.PositionGet()
+        len = int(self.player.GetMetadata()['mtime'])
+        pos += len/100*5*delta
+        self.player.PositionSet(pos)
+        self.redraw_pos(pos=pos/len)
 
     def Prev(self):
         self.player.Prev()
@@ -92,11 +128,6 @@ class PyMCApplet(plasma.Applet):
     def Next(self):
         self.player.Next()
 
-#    def shape(self):
-#        path = QPainterPath()
-#        path.addEllipse(self.boundingRect().adjusted(-2, -2, 2, 2))
-#        return path
-#
     def constraintsEvent(self, constraints):
         if constraints & Plasma.SizeConstraint:
             self.resize(self.size())
